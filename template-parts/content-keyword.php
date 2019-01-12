a3f5042602f9e93ed6f8c232a9eb89e93ee2be1a
jo-315 <32921100+jo-315@users.noreply.github.com>
2019-01-12 18:52:03 +0900

    途中コミット

---
+++ template-parts/content-keyword.php
@@ -0,0 +1,18 @@
+<div class="keyword_block_wrapper">
+  <div class="keyword_block_title">
+    pick up
+  </div>
+  <ul>
+    <li>
+      <a href="#">
+        <div>
+          <img
+            src="http://nonmonedev.local/wp-content/uploads/2018/12/image8.jpeg"
+            alt="海外"
+          />
+        </div>
+        <h5>海外</h5>
+      </a>
+    </li>
+  </ul>
+</div>

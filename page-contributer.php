454848a97383e713001a76ae1ca76eb7a6ad3e03
jo-315 <32921100+jo-315@users.noreply.github.com>
2019-03-23 16:54:50 +0900

    tmp commit

--- page-contributer.php
+++ page-contributer.php
@@ -40,6 +40,63 @@ get_header();
         </div>
       </div>
     </div>
+
+    <div class="member_content contributer_content">
+      <div class="profile_text_title">
+        <span>岡松太史</span>
+      </div>
+
+      <div class="member_img">
+        <img
+          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_taishi.png"
+          class="profile_img"
+        />
+      </div>
+
+      <div class="profile_text">
+        <div class="profile_text_content">
+        神戸市出身 1996年8月30日生まれ 二級建築士 2018年ProjectsAbroad タンザニア2020年タンザニアに移住予定 アフリカの人達を建築で救う アフリカ大陸全て制覇 日本各地でボランティア活動 CNSC〜Original Style〜YouTube活動中 小 中に授業、講演 オリジナルTシャツ完成 《Come true, Never give up, Smile, Connection》
+『笑顔を忘れず人との繋がりを大事にし諦めず本気で夢を追い続ければどんなにデカイ夢でも絶対に叶う
+        </div>
+
+        <div class="profile_text_content-button-wrap">
+        <a href='/author/taishi' class="profile_text_content-button">記事一覧</a>
+          <a
+            href="http://twitter.com/okamatutaishi"
+            target="_blank"
+            class="sns-social-wrap profile-sns-social-wrap "
+          >
+            <img
+              src="https://moire.xsrv.jp/wp-content/uploads/Twitter_Social_Icon_Rounded_Square_Color.png"
+              alt="Twitter"
+              class="profile-sns-social-icon"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/cnsc_originalstyle"
+            target="_blank"
+            class="sns-social-wrap profile-sns-social-wrap "
+          >
+            <img
+              src="https://moire.xsrv.jp/wp-content/uploads/IG_Glyph_Fill.psd-8.png"
+              alt="instagram"
+              class="profile-sns-social-icon"
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100010531653477"
+            target="_blank"
+            class="sns-social-wrap profile-sns-social-wrap "
+          >
+            <img
+              src="https://moire.xsrv.jp/wp-content/uploads/f-ogo_RGB_HEX-100.png"
+              alt="Facebook"
+              class="profile-sns-social-icon"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 

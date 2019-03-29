9446357b159f0692ed26517714ef0c1814adba10
jo-315 <32921100+jo-315@users.noreply.github.com>
2019-03-29 18:07:26 +0900

    tmp commit

--- page-contributer.php
+++ page-contributer.php
@@ -7,96 +7,104 @@ get_header();
     寄稿者一覧
   </h2>
   <div class="member_content_wrap">
-    <div class="member_content contributer_content">
-      <div class="profile_text_title">
-        <span>けーじ</span>
-      </div>
 
-      <div class="member_img">
-        <img
-          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_keiji.png"
-          class="profile_img"
-        />
-      </div>
+  <?php
+    $meta_query_args = array(
+    'relation' => 'AND',
+      array(
+        'key'     => 'position',
+        'value'   => '寄稿',
+        'compare' => '='
+      )
+    );
+    $users = get_users( array(
+      'meta_query' => $meta_query_args,
+      'exclude' => '',
+      'orderby'  => 'meta_value',
+      'meta_key' => 'position',
+    ));
 
-      <div class="profile_text">
-        <div class="profile_text_content">
-        京都府出身。1993年4月1日生まれ。大学在学時に国際ボランティアNGO・NICEと出会う。ワークキャンプや1年間のプログラム「ぼらいやー」に参加したほか、熊本地震の復興活動でNICEスタッフとしてボランティアコーディネートに従事。香川県のアートの島・豊島で1年間過ごした後、現在は熊本県南阿蘇村に在住。「一歩踏み出したい人の後押しがしたい」と思いつつ日々修行中。野球と音楽が好きです。
+    foreach($users as $user) {
+        $userId = $user->ID;
+        $name = get_the_author_meta('user_nicename', $userId);
+        $description = get_the_author_meta( 'user_description', $userId );
+?>
+
+        <div class="member_content contributer_content">
+          <div class="profile_text_title">
+            <span><?php echo the_author_meta( 'position', $userId ); ?>　<?php echo the_author_meta( 'display_name', $userId ); ?></span>
+          </div>
+
+          <div class="member_img">
+            <img
+            src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_<?php echo $name; ?>.png"
+              class="profile_img"
+            />
+          </div>
+
+          <div class="profile_text">
+            <?php if ($description !== '') { ?>
+              <div class="profile_text_content">
+                <?php echo $description; ?>
+              </div>
+            <?php }	?>
+
+            <div class="profile_text_content-button-wrap">
+              <a href='/author/<?php echo $name; ?>' class="profile_text_content-button">記事一覧</a>
+
+              <?php $twitter = get_the_author_meta( 'twitter', $userId ); ?>
+              <?php if ($twitter !== '') { ?>
+                <a
+                  href=<?php echo $twitter; ?>
+                  target="_blank"
+                  class="sns-social-wrap profile-sns-social-wrap"
+                >
+                  <img
+                    src="https://moire.xsrv.jp/wp-content/uploads/Twitter_Social_Icon_Rounded_Square_Color.png"
+                    alt="Twitter"
+                    class="profile-sns-social-icon"
+                  />
+                </a>
+              <?php } ?>
+
+              <?php $instagram = get_the_author_meta( 'instagram', $userId ); ?>
+              <?php if ($instagram !== '') { ?>
+                <a
+                  href="<?php echo $instagram; ?>"
+                  target="_blank"
+                  class="sns-social-wrap profile-sns-social-wrap "
+                >
+                  <img
+                    src="https://moire.xsrv.jp/wp-content/uploads/IG_Glyph_Fill.psd-8.png"
+                    alt="instagram"
+                    class="profile-sns-social-icon"
+                  />
+                </a>
+              <?php } ?>
+
+              <?php $facebook = get_the_author_meta( 'facebook', $userId ); ?>
+              <?php if ($facebook !== '') { ?>
+                <a
+                  href="<?php echo $facebook; ?>"
+                  target="_blank"
+                  class="sns-social-wrap profile-sns-social-wrap "
+                >
+                  <img
+                    src="https://moire.xsrv.jp/wp-content/uploads/f-ogo_RGB_HEX-100.png"
+                    alt="Facebook"
+                    class="profile-sns-social-icon"
+                  />
+                </a>
+              <?php } ?>
+              </a>
+            </div>
+          </div>
         </div>
 
-        <div class="profile_text_content-button-wrap">
-          <a href='/author/keiji' class="profile_text_content-button">記事一覧</a>
-          <a
-            href="https://www.facebook.com/keiji.miyamoto"
-            target="_blank"
-            class="sns-social-wrap profile-sns-social-wrap "
-          >
-            <img
-              src="https://moire.xsrv.jp/wp-content/uploads/f-ogo_RGB_HEX-100.png"
-              alt="Facebook"
-              class="profile-sns-social-icon"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
+<?php
+  };
+?>
 
-    <div class="member_content contributer_content">
-      <div class="profile_text_title">
-        <span>岡松太史</span>
-      </div>
-
-      <div class="member_img">
-        <img
-          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_taishi.png"
-          class="profile_img"
-        />
-      </div>
-
-      <div class="profile_text">
-        <div class="profile_text_content">
-        神戸市出身 1996年8月30日生まれ 二級建築士 2018年ProjectsAbroad タンザニア2020年タンザニアに移住予定 アフリカの人達を建築で救う アフリカ大陸全て制覇 日本各地でボランティア活動 CNSC〜Original Style〜YouTube活動中 小 中に授業、講演 オリジナルTシャツ完成 《Come true, Never give up, Smile, Connection》
-『笑顔を忘れず人との繋がりを大事にし諦めず本気で夢を追い続ければどんなにデカイ夢でも絶対に叶う』
-        </div>
-
-        <div class="profile_text_content-button-wrap">
-        <a href='/author/taishi' class="profile_text_content-button">記事一覧</a>
-          <a
-            href="http://twitter.com/okamatutaishi"
-            target="_blank"
-            class="sns-social-wrap profile-sns-social-wrap "
-          >
-            <img
-              src="https://moire.xsrv.jp/wp-content/uploads/Twitter_Social_Icon_Rounded_Square_Color.png"
-              alt="Twitter"
-              class="profile-sns-social-icon"
-            />
-          </a>
-          <a
-            href="https://www.instagram.com/cnsc_originalstyle"
-            target="_blank"
-            class="sns-social-wrap profile-sns-social-wrap "
-          >
-            <img
-              src="https://moire.xsrv.jp/wp-content/uploads/IG_Glyph_Fill.psd-8.png"
-              alt="instagram"
-              class="profile-sns-social-icon"
-            />
-          </a>
-          <a
-            href="https://www.facebook.com/profile.php?id=100010531653477"
-            target="_blank"
-            class="sns-social-wrap profile-sns-social-wrap "
-          >
-            <img
-              src="https://moire.xsrv.jp/wp-content/uploads/f-ogo_RGB_HEX-100.png"
-              alt="Facebook"
-              class="profile-sns-social-icon"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
   </div>
 </div>
 
--- page-member.php
+++ page-member.php
@@ -1,11 +1,4 @@
 <?php
-/*
-カテゴリー毎、投稿一覧
-blog,event,orgで同じもの
-*/
-?>
-
-<?php
 get_header();
 ?>
 
@@ -14,225 +7,108 @@ get_header();
     メンバー一覧
   </h2>
   <div class="member_content_wrap">
-    <div class="member_content">
-      <div class="profile_text_title">
-        <span>moire代表　　見城 俊介</span>
-      </div>
 
-      <div class="member_img">
-        <img
-          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_kenjo.png"
-          class="profile_img"
-        />
-      </div>
+  <?php
+    $positions = array('moire代表', 'moire運営メンバー', 'ライター');
 
-      <div class="profile_text">
-        <div class="profile_text_content">
-          大学に入ってすぐNICE、ひいてはボランティアに関わるように。
-          二回生の冬からはBaseconnect Incでエンジニアインターンに参加。
-          現在はmoireやNICEの活動を行いつつインターンしてます。
-        </div>
+    foreach($positions as $position) {
+      $meta_query_args = array(
+      'relation' => 'AND',
+        array(
+          'key'     => 'position',
+          'value'   => $position,
+          'compare' => '='
+        )
+      );
+      $users = get_users( array(
+        'meta_query' => $meta_query_args,
+        'exclude' => '',
+        'orderby'  => 'meta_value',
+        'meta_key' => 'position',
+      ));
 
-        <div class="profile_text_content-button-wrap">
-          <a href='/author/kenjo' class="profile_text_content-button">記事一覧</a>
-          <a
-            href="https://twitter.com/VYZ3pDOkmPiwXtf?lang=ja"
-            target="_blank"
-            class="sns-social-wrap profile-sns-social-wrap "
-          >
-            <img
-              src="https://moire.xsrv.jp/wp-content/uploads/Twitter_Social_Icon_Rounded_Square_Color.png"
-              alt="Twitter"
-              class="profile-sns-social-icon"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
+      foreach($users as $user) {
+          $userId = $user->ID;
+          $name = get_the_author_meta('user_nicename', $userId);
+          $description = get_the_author_meta( 'user_description', $userId );
+  ?>
 
-    <div class="member_content">
-      <div class="profile_text_title">
-        <span>moire運営メンバー　　神田 勇太 (よんて)</span>
-      </div>
+          <div class="member_content">
+            <div class="profile_text_title">
+              <span><?php echo the_author_meta( 'position', $userId ); ?>　<?php echo the_author_meta( 'display_name', $userId ); ?></span>
+            </div>
 
-      <div class="member_img">
-        <img
-          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_yonta.png"
-          class="profile_img"
-        />
-      </div>
+            <div class="member_img">
+              <img
+              src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_<?php echo $name; ?>.png"
+                class="profile_img"
+              />
+            </div>
 
-      <div class="profile_text">
-        <div class="profile_text_content">
-          絶賛就活中</br>
-          追われるより追いたいタイプ</br>
-          現在は反抗期の妹とペットのホルスタインに悩まされてます。
-        </div>
-        <div class="profile_text_content-button-wrap">
-          <a href='/author/yonta' class="profile_text_content-button">記事一覧</a>
-        </div>
-      </div>
-    </div>
+            <div class="profile_text">
+              <?php if ($description !== '') { ?>
+                <div class="profile_text_content">
+                  <?php echo $description; ?>
+                </div>
+              <?php }	?>
 
-    <div class="member_content">
-      <div class="profile_text_title">
-        <span>ライター　　ナターシャ</span>
-      </div>
+              <div class="profile_text_content-button-wrap">
+                <a href='/author/<?php echo $name; ?>' class="profile_text_content-button">記事一覧</a>
 
-      <div class="member_img">
-        <img
-          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_natalie.png"
-          class="profile_img"
-        />
-      </div>
+                <?php $twitter = get_the_author_meta( 'twitter', $userId ); ?>
+                <?php if ($twitter !== '') { ?>
+                  <a
+                    href=<?php echo $twitter; ?>
+                    target="_blank"
+                    class="sns-social-wrap profile-sns-social-wrap"
+                  >
+                    <img
+                      src="https://moire.xsrv.jp/wp-content/uploads/Twitter_Social_Icon_Rounded_Square_Color.png"
+                      alt="Twitter"
+                      class="profile-sns-social-icon"
+                    />
+                  </a>
+                <?php } ?>
 
-      <div class="profile_text">
-        <div class="profile_text_content">
-          中の人は日本人です。現在ロシアのサンクト=ペテルブルクに留学中。普通の大学3年生。太陽の光を浴びたいが最近の口癖。よろしくお願いいたします。
-        </div>
-        <div class="profile_text_content-button-wrap">
-          <a href='/author/natalie' class="profile_text_content-button">記事一覧</a>
-        </div>
-      </div>
-    </div>
+                <?php $instagram = get_the_author_meta( 'instagram', $userId ); ?>
+                <?php if ($instagram !== '') { ?>
+                  <a
+                    href="<?php echo $instagram; ?>"
+                    target="_blank"
+                    class="sns-social-wrap profile-sns-social-wrap "
+                  >
+                    <img
+                      src="https://moire.xsrv.jp/wp-content/uploads/IG_Glyph_Fill.psd-8.png"
+                      alt="instagram"
+                      class="profile-sns-social-icon"
+                    />
+                  </a>
+                <?php } ?>
 
-    <div class="member_content">
-      <div class="profile_text_title">
-        <span>ライター　　小香</span>
-      </div>
+                <?php $facebook = get_the_author_meta( 'facebook', $userId ); ?>
+                <?php if ($facebook !== '') { ?>
+                  <a
+                    href="<?php echo $facebook; ?>"
+                    target="_blank"
+                    class="sns-social-wrap profile-sns-social-wrap "
+                  >
+                    <img
+                      src="https://moire.xsrv.jp/wp-content/uploads/f-ogo_RGB_HEX-100.png"
+                      alt="Facebook"
+                      class="profile-sns-social-icon"
+                    />
+                  </a>
+                <?php } ?>
+                </a>
+              </div>
+            </div>
+          </div>
 
-      <div class="member_img">
-        <img
-          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_shoka.png"
-          class="profile_img"
-        />
-      </div>
+ <?php
+    };
+   };
+  ?>
 
-      <div class="profile_text">
-        <div class="profile_text_content">
-          犬を愛する旅行好きな中国語学習者。ただ今中国にて修行中です。老後は犬とともに暮らしたい。
-        </div>
-        <div class="profile_text_content-button-wrap">
-          <a href='/author/shoka' class="profile_text_content-button">記事一覧</a>
-        </div>
-      </div>
-    </div>
-
-    <div class="member_content">
-      <div class="profile_text_title">
-        <span>ライター　　タロ</span>
-      </div>
-
-      <div class="member_img">
-        <img
-          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_taro.png"
-          class="profile_img"
-        />
-      </div>
-
-      <div class="profile_text">
-        <div class="profile_text_content">
-          ドイツに留学中</br>
-          本場ドイツでビールを飲むも普通に「苦っ」と言ってしまう。ただ、Radler という飲みやすいビールに出会い救われる。
-        </div>
-        <div class="profile_text_content-button-wrap">
-          <a href='/author/taro' class="profile_text_content-button">記事一覧</a>
-        </div>
-      </div>
-    </div>
-
-    <div class="member_content">
-      <div class="profile_text_title">
-        <span>ライター　　今西 雄飛</span>
-      </div>
-
-      <div class="member_img">
-        <img
-          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_imanishi.png"
-          class="profile_img"
-        />
-      </div>
-
-      <div class="profile_text">
-        <div class="profile_text_content">
-          1番心惹かれた国はカンボジア🇰🇭</br>
-          現在はカンボジア農村部で日本語を通して子供たちの職業選択肢の幅を広げる取り組みを行っています。
-        </div>
-
-        <div class="profile_text_content-button-wrap">
-          <a href='/author/imanishi' class="profile_text_content-button">記事一覧</a>
-          <a
-            href="https://twitter.com/imanishi?lang=ja"
-            target="_blank"
-            class="sns-social-wrap profile-sns-social-wrap "
-          >
-            <img
-              src="https://moire.xsrv.jp/wp-content/uploads/Twitter_Social_Icon_Rounded_Square_Color.png"
-              alt="Twitter"
-              class="profile-sns-social-icon"
-            />
-          </a>
-          <a
-            href="https://www.instagram.com/yuhimanishi/"
-            target="_blank"
-            class="sns-social-wrap profile-sns-social-wrap "
-          >
-            <img
-              src="https://moire.xsrv.jp/wp-content/uploads/IG_Glyph_Fill.psd-8.png"
-              alt="instagram"
-              class="profile-sns-social-icon"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
-
-    <div class="member_content">
-      <div class="profile_text_title">
-        <span>ライター　　あゆみ</span>
-      </div>
-
-      <div class="member_img">
-        <img
-          src="https://moire.xsrv.jp/wp-content/uploads/profile/profile_ayumi.png"
-          class="profile_img"
-        />
-      </div>
-
-      <div class="profile_text">
-        <div class="profile_text_content">
-        大学3年時に休学してNICEのぼらいやーに参加。
-        国際協力にはまり、国際NPOのテラ・ルネッサンスにてインターンとして活動中。
-        （スイーツと東南アジアをこよなく愛する大学生。）
-        </div>
-
-        <div class="profile_text_content-button-wrap">
-          <a href='/author/ayumi' class="profile_text_content-button">記事一覧</a>
-          <a
-            href="https://twitter.com/ayumi_0311"
-            target="_blank"
-            class="sns-social-wrap profile-sns-social-wrap "
-          >
-            <img
-              src="https://moire.xsrv.jp/wp-content/uploads/Twitter_Social_Icon_Rounded_Square_Color.png"
-              alt="Twitter"
-              class="profile-sns-social-icon"
-            />
-          </a>
-          <a
-            href="https://www.instagram.com/1030ayumi0311/?hl=ja"
-            target="_blank"
-            class="sns-social-wrap profile-sns-social-wrap "
-          >
-            <img
-              src="https://moire.xsrv.jp/wp-content/uploads/IG_Glyph_Fill.psd-8.png"
-              alt="instagram"
-              class="profile-sns-social-icon"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
   </div>
 </div>
 
